81a5bc6ced82afb7385ffd88b494af2a1cd9b952
Md. Tauhidul Islam <tauhid.it200@gmail.com>
2017-04-15 02:11:42 +0600

    Modified default login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -50,9 +50,6 @@
 
                         <div class="form-group">
                             <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Login
-                                </button>
 
                                 <a class="btn btn-link" href="{{ url('/password/reset') }}">
                                     Forgot Your Password?
