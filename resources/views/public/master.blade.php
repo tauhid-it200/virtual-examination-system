eb2d03db705e31e55cec0a693b64b4008ae1a85c
Md. Tauhidul Islam <tauhid.it200@gmail.com>
2017-05-01 21:19:43 +0600

    HTML::style is given a try

--- resources/views/public/master.blade.php
+++ resources/views/public/master.blade.php
@@ -11,7 +11,7 @@
         <title>Virtual Examination System</title>
 
         <!-- Styles -->
-        <link href="{{secure_asset("admin/vendor/bootstrap/css/bootstrap.min.css")}}" rel="stylesheet">
+        <link href="{{HTML::style("admin/vendor/bootstrap/css/bootstrap.min.css", array(), true)}}" rel="stylesheet">
         <!--<link href="/admin/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">-->
 
         <!-- Scripts -->
