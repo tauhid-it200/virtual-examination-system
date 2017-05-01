031e0371283d1e800c5910d023adc34caafd05e5
Md. Tauhidul Islam <tauhid.it200@gmail.com>
2017-05-02 00:41:47 +0600

    AppServiceProvider updated

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,7 @@ class AppServiceProvider extends ServiceProvider {
     public function boot() {
 //        $this->app['request']->server->set('HTTPS', $this->app->environment() != 'local');
 //        if (env('APP_ENV') === 'production') {
-            URL::forceSchema('https');
+            
 //        }
     }
 
