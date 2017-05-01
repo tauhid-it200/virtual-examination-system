b3cb7b343df672e28206cd06df3cd60ca2b19b84
Md. Tauhidul Islam <tauhid.it200@gmail.com>
2017-05-02 00:59:02 +0600

    .env updated

--- config/app.php
+++ config/app.php
@@ -45,7 +45,7 @@ return [
       | your application so that it is used when running Artisan tasks.
       |
      */
-    'url' => env('APP_URL', 'https://virtualexam.herokuapp.com'),
+    'url' => env('APP_URL', 'http://localhost'),
     /*
       |--------------------------------------------------------------------------
       | Application Timezone
