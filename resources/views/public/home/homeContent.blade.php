7453bed8d5e52104b5cfc180e896ccfe94918973
Md. Tauhidul Islam <tauhid.it200@gmail.com>
2017-05-01 10:39:08 +0600

    Login As Admin button added

--- resources/views/public/home/homeContent.blade.php
+++ resources/views/public/home/homeContent.blade.php
@@ -4,6 +4,7 @@
 <div class="row">
     <div class="col-lg-12">
         <h1 class="page-header">Welcome to Virtual Examination System</h1>
+        <a href="{{url("/login")}}" class="button button-info"></a>
     </div>
 </div>
 
