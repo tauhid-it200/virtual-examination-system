f37778a2b2ce9c8d06fbb668774f0a3692a2fc1c
Md. Tauhidul Islam <tauhid.it200@gmail.com>
2017-05-01 11:01:05 +0600

    URL secure attempt 01

--- resources/views/public/home/homeContent.blade.php
+++ resources/views/public/home/homeContent.blade.php
@@ -4,7 +4,7 @@
 <div class="row">
     <div class="col-lg-12">
         <h1 class="page-header">Welcome to Virtual Examination System</h1>
-        <a href="{{url("/login")}}" class="button button-info">Login As Admin</a>
+        <a href="{{secure_url("/login")}}" class="button button-info">Login As Admin</a>
     </div>
 </div>
 
