63d5738d565fb301746ccf3437b7d5e6dfdffd04
Md. Tauhidul Islam <tauhid.it200@gmail.com>
2017-05-01 13:01:31 +0600

    login.blade.php modified

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,66 +1,99 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading"></div>
-                <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
-                        {{ csrf_field() }}
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+        <title>Virtual Examination System</title>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+        <!-- Styles -->
+        <link href="{{asset("admin/vendor/bootstrap/css/bootstrap.min.css")}}" rel="stylesheet">
 
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+        <!-- Scripts -->
+        <script>
+            window.Laravel = <?php
+echo json_encode([
+    'csrfToken' => csrf_token(),
+]);
+?>
+        </script>
+    </head>
+    <body>
+        <nav class="navbar navbar-default navbar-static-top center-block" style="height: 100px; background-color: #330000;">
+            <div class="container">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="{{ url('/login') }}">
+                        <h3 style="color: wheat;">Virtual Examination System</h3>
+                    </a>
+                </div>
+            </div>
+        </nav>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 col-md-offset-3">
+                    <div class="panel panel-default">
+                        <div class="panel-heading text-center" style="background-color: #666666;">
+                            <h4 class="text-center" style="color: white;">Please Login With Valid Email Address & Password</h4>
                         </div>
+                        <div class="panel-body">
+                            <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
+                                {!!Form::open(array("url"=>"/login", "role"=>"form", "method"=>"POST"))!!}
+                                <fieldset>
+                                    <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                                        <label for="email" class="control-label col-md-4">E-Mail Address</label>
+                                        <div class="col-md-8">
+                                            <input class="form-control" id="email" name="email" type="email"/>
+                                            @if ($errors->has('email'))
+                                            <span class="help-block">
+                                                <strong>{{ $errors->first('email') }}</strong>
+                                            </span>
+                                            @endif
+                                        </div>
+                                    </div>
 
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
+                                    <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                                        <label for="password" class="control-label col-md-4">Password</label>
+                                        <div class="col-md-8">
+                                            <input class="form-control" id="password" name="password" type="password"/>
+                                            @if ($errors->has('password'))
+                                            <span class="help-block">
+                                                <strong>{{ $errors->first('password') }}</strong>
+                                            </span>
+                                            @endif
+                                        </div>
+                                    </div>
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
+                                    <div class="form-group">
+                                            <div class="checkbox col-md-offset-4 col-md-4">
+                                                <label>
+                                                    <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : ''}}> Remember Me
+                                                </label>
+                                            </div>
+                                            <div class="col-md-4">
+                                                <a class="btn btn-link" href="{{ url('/password/reset') }}">
+                                                    Forgot Your Password?
+                                                </a>
+                                            </div>
+                                    </div>
 
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+                                    <div class="form-group">
+                                        <div class="col-md-8 col-md-offset-4">
+                                            <button type="submit" class="btn btn-primary btn-block">
+                                                Login
+                                            </button>
+                                        </div>
+                                    </div>
+                                </fieldset>
+                                {!!Form::close()!!}
                         </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : ''}}> Remember Me
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-								<button type="submit" class="btn btn-primary btn-block">Login 
-                                </button>
-                                <a class="btn btn-link" href="{{ url('/password/reset') }}">
-                                    Forgot Your Password?
-                                </a>
-                            </div>
-                        </div>
-                    </form>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</div>
-@endsection
+
+    </body>
+</html>
